904a2c3e431610dcb1cadd97dace3f8503d1a809
leesanghyuk2000 <leesanghyuk2000@gmail.com>
2025-04-14 22:45:20 +0900

    feat: add new algorithm

--- Baekjoon/Array/10818/solution2.cpp
+++ Baekjoon/Array/10818/solution2.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
@@ -7,5 +9,24 @@ int main(){
     cin.tie(NULL);
     cout.tie(NULL);
 
+    unsigned int N;
+
+    cin >> N;
+
+    vector<int> vec(N);
+
+    for (int i=0; i<N; i++){
+        cin >> vec[i];
+    }
+
+    auto min_valp = min_element(vec.begin(), vec.end());
+    int min_val = *min_valp;
+
+    auto max_valp = max_element(vec.begin(), vec.end());
+    int max_val = *max_valp;
+
+    cout << min_val << " " << max_val << "\n";
+
+    return 0;
     
 }

9a04ce194f89afc999669789df0f8b9d2ef0bfe6
Lee Sanghyuk <leesanghyuk2000@gmail.com>
2025-04-26 22:43:18 +0900

    Second commit from 7800

--- Baekjoon/Bruteforce/1018/solution.py
+++ Baekjoon/Bruteforce/1018/solution.py
@@ -1,9 +1,27 @@
-import sys
-
-N, M = map(int, sys.stdin.readline().split())
+N, M = map(int, input().split())
 
 arr = []
 for _ in range(N):
-    row = list(sys.stdin.readline().rstrip())
-    arr.append(row)
+    arr.append(input())
 
+cnt = []
+for a in range(N-7):
+    for b in range(M-7):
+        idx1=0 # 'W'와 다른거 count
+        idx2=0 # 'B'와 다른거 count
+        for i in range(a, a+8):
+            for j in range(b, b+8):
+                if (i+j) % 2 == 0: # 두 인덱스의 합이 짝수라면 0, 0과 같아야 함
+                    if arr[i][j] != 'W':
+                        idx1 += 1
+                    if arr[i][j] != 'B':
+                        idx2 += 1
+                else: # 두 인덱스의 합이 홀수라면, 0, 0과 달라야 함
+                    if arr[i][j] != 'W':
+                        idx2 += 1
+                    if arr[i][j] != 'B':
+                        idx1 += 1
+
+        cnt.append(min(idx1, idx2))
+
+print(min(cnt))
